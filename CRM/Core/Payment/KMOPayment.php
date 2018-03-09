54c54815913e923e3224db0eab3766078c10436e
wouter.h <wouter@ctrl.be>
2018-03-09 16:57:52 +0100

    refs #code added files.

---
+++ CRM/Core/Payment/KMOPayment.php
@@ -0,0 +1,84 @@
+<?php
+
+require_once 'CRM/Core/Payment.php';
+
+use CRM_Kmowallet_ExtensionUtil as E;
+
+class CRM_Core_Payment_KMOPayment extends CRM_Core_Payment {
+
+  /**
+   * We only need one instance of this object. So we use the singleton
+   * pattern and cache the instance in this variable
+   *
+   * @var object
+   * @static
+   */
+  static private $_singleton = NULL;
+
+  /**
+   * mode of operation: live or test
+   *
+   * @var object
+   * @static
+   */
+  static protected $_mode = NULL;
+
+  /**
+   * Constructor
+   *
+   * @param string $mode the mode of operation: live or test
+   *
+   * @return void
+   */
+  function __construct($mode, &$paymentProcessor) {
+    $this->_mode = $mode;
+    $this->_paymentProcessor = $paymentProcessor;
+    $this->_processorName = E::ts('KMO Wallet', ['domain' => 'be.ctrl.kmowallet']);
+  }
+
+  /**
+   * singleton function used to manage this object
+   *
+   * @param string $mode the mode of operation: live or test
+   *
+   * @return object
+   * @static
+   *
+   */
+  static function &singleton($mode, &$paymentProcessor) {
+    $processorName = $paymentProcessor['name'];
+    if (self::$_singleton[$processorName] === NULL) {
+      self::$_singleton[$processorName] = new CRM_Core_Payment_KMOPayment($mode, $paymentProcessor);
+    }
+    return self::$_singleton[$processorName];
+  }
+
+  /**
+   * This function checks to see if we have the right config values
+   *
+   * @return string the error message if any
+   * @public
+   */
+  function checkConfig() {
+    return NULL;
+  }
+
+  function doDirectPayment(&$params) {
+
+  }
+
+  /**
+   * Sets appropriate parameters for checking out KMO wallet
+   *
+   * @param array $params name value pair of contribution data
+   *
+   * @return void
+   * @access public
+   *
+   */
+  function doTransferCheckout(&$params, $component) {
+    // Message.
+    $message = E::ts("Process your payment with the 'kmo-portefeuille'.", ['domain' => 'be.ctrl.kmowallet']);
+    CRM_Core_Session::setStatus($message, '', 'success');
+  }
+}
---
+++ kmowallet.php
@@ -0,0 +1,149 @@
+<?php
+
+require_once 'kmowallet.civix.php';
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function kmowallet_civicrm_config(&$config) {
+  _kmowallet_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function kmowallet_civicrm_xmlMenu(&$files) {
+  _kmowallet_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function kmowallet_civicrm_install() {
+  _kmowallet_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
+ */
+function kmowallet_civicrm_postInstall() {
+  _kmowallet_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function kmowallet_civicrm_uninstall() {
+  _kmowallet_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function kmowallet_civicrm_enable() {
+  _kmowallet_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function kmowallet_civicrm_disable() {
+  _kmowallet_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function kmowallet_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _kmowallet_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function kmowallet_civicrm_managed(&$entities) {
+  $entities[] = [
+    'module' => 'be.ctrl.kmowallet',
+    'name' => 'KMO Wallet',
+    'entity' => 'PaymentProcessorType',
+    'params' => [
+      'version' => 3,
+      'name' => 'KMO_wallet',
+      'title' => 'KMO wallet payment',
+      'description' => 'KMO wallet payment option.',
+      'class_name' => 'Payment_KMOPayment',
+      'billing_mode' => 'notify',
+      'user_name_label' => 'Username',
+      'is_recur' => 0,
+      'payment_type' => 1,
+    ],
+  ];
+  $entities[] = [
+    'module' => 'be.ctrl.kmowallet',
+    'entity' => 'OptionValue',
+    'params' => [
+      'version' => 3,
+      'option_group_id' => 'payment_instrument',
+      'label' => 'KMO',
+      'name' => 'KMO',
+    ],
+  ];
+  _kmowallet_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function kmowallet_civicrm_caseTypes(&$caseTypes) {
+  _kmowallet_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+ */
+function kmowallet_civicrm_angularModules(&$angularModules) {
+  _kmowallet_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function kmowallet_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _kmowallet_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
